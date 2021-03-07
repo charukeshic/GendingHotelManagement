8cfd34601034327cb809a73a2ff89da5c5861497
CaiYingChau <77651815+CaiYingChau@users.noreply.github.com>
2021-03-07 23:40:50 +0800

    Committed by Caiying

---
+++ app/src/main/java/com/example/gendinghotelmanagement/Login.kt
@@ -0,0 +1,56 @@
+package com.example.gendinghotelmanagement
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+
+class Login : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+
+
+    lateinit var toolbar:Toolbar
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navView:NavigationView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navView = findViewById(R.id.nav_view)
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar ,  0, 0
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        navView.setNavigationItemSelectedListener (this)
+
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem):Boolean{
+        when (item.itemId){
+            R.id.sign_in -> {
+                Toast.makeText(this,"{Sign in clicked",Toast.LENGTH_SHORT).show()
+            }
+
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+
+}
