192e80c1dceff7c4f864a79548badf7989cfe024
Wei Meng <ngwm-wp18@student.tarc.edu.my>
2021-04-16 18:39:54 +0800

    can modify profile except password

--- app/src/main/java/com/example/gendinghotelmanagement/EditProfile.kt
+++ app/src/main/java/com/example/gendinghotelmanagement/EditProfile.kt
@@ -1,17 +1,20 @@
 package com.example.gendinghotelmanagement
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 
 class EditProfile : AppCompatActivity() , NavigationView.OnNavigationItemSelectedListener {
 
@@ -20,6 +23,18 @@ class EditProfile : AppCompatActivity() , NavigationView.OnNavigationItemSelecte
     lateinit var navView: NavigationView
 
     lateinit var btnUpdateProfile: Button
+
+    lateinit var txtEmail: EditText
+    lateinit var txtRole: EditText
+    lateinit var txtName: EditText
+    lateinit var txtAddress: EditText
+    lateinit var txtPassword: EditText
+    lateinit var txtConPassword: EditText
+    lateinit var txtPhone: EditText
+
+
+    lateinit var databaseUser: DatabaseReference
+    lateinit var reff: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,53 +48,137 @@ class EditProfile : AppCompatActivity() , NavigationView.OnNavigationItemSelecte
         val username=intent.getStringExtra("Username")
 
         val toggle = ActionBarDrawerToggle(
-                this, drawerLayout, toolbar, 0, 0
+            this, drawerLayout, toolbar, 0, 0
         )
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
         navView.setNavigationItemSelectedListener(this)
 
+        txtEmail = findViewById(R.id.txtEmail)
+        txtRole = findViewById(R.id.txtRole)
+        txtName = findViewById(R.id.txtName)
+        txtAddress = findViewById(R.id.txtAddress)
+        txtPhone = findViewById(R.id.txtPhone)
+
+        val findUser = username.toString().replace('.', '-')
+
+        reff = findUser?.let { FirebaseDatabase.getInstance().getReference("User").child(it) }!!;
+
+        reff.addValueEventListener(object : ValueEventListener {
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val email = snapshot.child("email").getValue().toString();
+                val role = snapshot.child("role").getValue().toString();
+                val name = snapshot.child("name").getValue().toString();
+                val address = snapshot.child("address").getValue().toString();
+                val phone = snapshot.child("phone").getValue().toString();
+                val password = snapshot.child("password").getValue().toString();
+                val userID = snapshot.child("userID").getValue().toString();
+
+                txtEmail.setText(email);
+                txtRole.setText(role);
+                txtName.setText(name);
+                txtAddress.setText(address);
+                txtPhone.setText(phone);
+            }
+
+
+        });
+
         btnUpdateProfile = findViewById(R.id.btnUpdateProfile);
         btnUpdateProfile.setOnClickListener { // Do some work here
-            val intent = Intent (this@EditProfile,ManagerStaffPortal::class.java)
-            intent.putExtra("Username",username)
+            val intent = Intent(this@EditProfile, ManagerStaffPortal::class.java)
+            intent.putExtra("Username", username)
             startActivity(intent);
+            saveProfile();
         }
+    }
+
+    private fun saveProfile(){
+        val txtEmail = findViewById<EditText>(R.id.txtEmail)
+        val txtRole = findViewById<EditText>(R.id.txtRole)
+        val txtName = findViewById<TextView>(R.id.txtName)
+        val txtAddress = findViewById<TextView>(R.id.txtAddress)
+        val txtPassword = findViewById<TextView>(R.id.txtPassword)
+        val txtConPassword = findViewById<TextView>(R.id.txtConPassword)
+        val txtPhone = findViewById<TextView>(R.id.txtPhone)
+
+        val email = txtEmail.text.toString().trim();
+        val role = txtRole.text.toString().trim();
+        val name = txtName.text.toString().trim();
+        val address = txtAddress.text.toString().trim();
+        val password = txtPassword.text.toString().trim();
+        val conPassword = txtConPassword.text.toString().trim();
+        val phone = txtPhone.text.toString().trim();
+
+        if(email.isEmpty()){
+            txtEmail.error ="Please enter an email"
+            return
+        }
+        if(role.isEmpty()){
+            txtRole.error ="Please enter an email"
+            return
+        }
+        if(name.isEmpty()){
+            txtName.error ="Please enter an email"
+            return
+        }
+        if(address.isEmpty()){
+            txtAddress.error ="Please enter an email"
+            return
+        }
+        if(phone.isEmpty()){
+            txtPhone.error ="Please enter an email"
+            return
+        }
+
+
+
+        reff.child("email").setValue(email)
+        reff.child("role").setValue(role)
+        reff.child("name").setValue(name)
+        reff.child("address").setValue(address)
+        reff.child("phone").setValue(phone)
+
     }
 
     override fun onNavigationItemSelected(item: MenuItem):Boolean{
         val username=intent.getStringExtra("Username")
         when (item.itemId){
             R.id.ic_profile -> {
-                val intent = Intent (this@EditProfile,ManagerStaffPortal::class.java)
-                intent.putExtra("Username",username)
+                val intent = Intent(this@EditProfile, ManagerStaffPortal::class.java)
+                intent.putExtra("Username", username)
                 startActivity(intent);
-                Toast.makeText(this,"{Profile clicked",Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "{Profile clicked", Toast.LENGTH_SHORT).show()
             }
             R.id.nav_booking -> {
-                val intent = Intent (this@EditProfile,OrderDetails::class.java)
-                intent.putExtra("Username",username)
+                val intent = Intent(this@EditProfile, OrderDetails::class.java)
+                intent.putExtra("Username", username)
                 startActivity(intent);
-                Toast.makeText(this,"{Booking clicked",Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "{Booking clicked", Toast.LENGTH_SHORT).show()
             }
             R.id.nav_activity -> {
-                val intent = Intent (this@EditProfile,CustomerActivity::class.java)
-                intent.putExtra("Username",username)
+                val intent = Intent(this@EditProfile, CustomerActivity::class.java)
+                intent.putExtra("Username", username)
                 startActivity(intent);
-                Toast.makeText(this,"{Customer Activity clicked",Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "{Customer Activity clicked", Toast.LENGTH_SHORT).show()
             }
             R.id.nav_operation -> {
-                val intent = Intent (this@EditProfile,CheckRoomOccupancy::class.java)
-                intent.putExtra("Username",username)
+                val intent = Intent(this@EditProfile, CheckRoomOccupancy::class.java)
+                intent.putExtra("Username", username)
                 startActivity(intent);
-                Toast.makeText(this,"{Operation clicked",Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "{Operation clicked", Toast.LENGTH_SHORT).show()
             }
             R.id.nav_logout -> {
                 FirebaseAuth.getInstance().signOut();
-                val intent = Intent (this@EditProfile,Login::class.java)
-                intent.putExtra("Username",username)
+                val intent = Intent(this@EditProfile, Login::class.java)
+                intent.putExtra("Username", username)
                 startActivity(intent);
-                Toast.makeText(this,"{You are successfully sign out",Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "{You are successfully sign out", Toast.LENGTH_SHORT).show()
             }
         }
         drawerLayout.closeDrawer(GravityCompat.START)
